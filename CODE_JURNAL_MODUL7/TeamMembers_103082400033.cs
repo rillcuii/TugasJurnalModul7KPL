388479415198f6069a0d71bace09b9c951c0f3b1
khalilgtgbgtkhalilgtg@gmail.com <khaaleell>
2026-04-19 00:13:09 +0700

    Menambahakan class TeamMembers_103082400033

---
+++ CODE_JURNAL_MODUL7/TeamMembers_103082400033.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace CODE_JURNAL_MODUL7
+{
+
+    public class members()
+    {
+        public string nim { get; set; }
+        public string namadepan { get; set; }
+        public string namabelakang { get; set; }
+        public string gender { get; set; }
+        public int umur { get; set; }
+    }
+    public class TeamMembers_103082400033
+    {
+        public List <members> members { get; set; }
+
+        public void readJSON()
+        {
+            string jsonString = File.ReadAllText("jurnal7_2_103082400033.json");
+            TeamMembers_103082400033 data = JsonSerializer.Deserialize<TeamMembers_103082400033>(jsonString);
+
+            Console.WriteLine("Team member list:");
+            foreach (var m in data.members)
+            {
+                Console.WriteLine($"{m.nim} {m.namadepan} {m.namabelakang} ({m.umur} {m.gender})");
+            }
+        }
+    }
+}
