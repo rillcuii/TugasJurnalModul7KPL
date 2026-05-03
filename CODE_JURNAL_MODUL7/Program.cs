08324635d2efa65cdb9b13099a52b0c945f5f520
andra <veryandra.putra@gmail.com>
2026-05-03 18:07:38 +0700

    final projek modul 7

--- CODE_JURNAL_MODUL7/Program.cs
+++ CODE_JURNAL_MODUL7/Program.cs
@@ -1,1 +1,28 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CODE_JURNAL_MODUL7  // Sesuaikan nama namespace kelompokmu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // --- TEST LEVEL 1 ---
+            Console.WriteLine("=== Hasil Deserialisasi JSON 1 ===");
+            DataMahasiswa103082400042 level1 = new DataMahasiswa103082400042();
+            level1.ReadJSON();
+            Console.WriteLine();
+
+            // --- TEST LEVEL 2 ---
+            Console.WriteLine("=== Hasil Deserialisasi JSON 2 ===");
+            TeamMembers103082400042 level2 = new TeamMembers103082400042();
+            level2.ReadJSON();
+            Console.WriteLine(); 
+
+            // --- TEST LEVEL 3 ---
+            Console.WriteLine("=== Hasil Deserialisasi JSON 3 ===");
+            GlossaryItem103082400042 level3 = new GlossaryItem103082400042();
+            level3.ReadJSON();
+            Console.WriteLine(); 
+        }
+    }
+}
